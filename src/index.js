4e8b4f65e5d7e22cf21bed102a67e379db091c66
Redona Kembora <kembor@usi.ch>
2019-02-02 21:32:06 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,143 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import clear from './images/clear.png';
+import clouds from './images/clouds.png';
+import rain from './images/rain.png';
+import snow from './images/snow.png';
+import thunderstorm from './images/thunderstorm.png'
+import mist from './images/mist.png'
+import snow_and_rain from './images/snow_and_rain.png'
+
+const API_KEY =`${process.env.REACT_APP_API_KEY}`;
+console.log(API_KEY)
+
+class WeatherApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false,
+            temperature: 0,
+            cityName: null,
+            isCelsius: true,
+            main: 0,
+            countryName: null,
+            weatherCondition: null,
+            error: null
+        }
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                this.fetchWeather(position.coords.latitude, position.coords.longitude);
+            },
+            error => {
+                this.setState({
+                    error: 'Error Gettig Weather Condtions'
+                });
+            }
+        );
+
+    }
+
+
+    fetchWeather(lat, lon) {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=beff0ff186778ab01de491a06beff296&units=metric`)
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    temperature: json.main.temp,
+                    main: json.weather[0].main,
+                    cityName: json.name,
+                    countryName: json.sys.country,
+                    weatherCondition: json.weather[0].description,
+                    isLoading: true,
+                })
+
+            });
+    }
+
+    handleClick() {
+        this.setState(function (prevState) {
+            return {isCelsius: !prevState.isCelsius};
+
+        });
+        if (this.state.isCelsius) {
+            this.setState({temperature: Math.round((this.state.temperature * (9 / 5)) + 32 * 100 / 100)})
+        } else {
+            this.setState({temperature: Math.round((this.state.temperature - 32) * 5 / 9 * 100) / 100})
+        }
+    }
+
+    render() {
+        var {isLoading, weatherCondition, cityName, countryName, temperature} = this.state;
+        var load = {textAlign: 'center'};
+        let weatherImage;
+
+        if (!isLoading) {
+            return <div style={load}>Loading...</div>;
+        }
+
+        console.log(weatherCondition)
+
+        if (weatherCondition === 'clear sky'){
+            weatherImage = <img src={clear} alt="clear"/>
+        }
+        if ((weatherCondition === "few clouds") || (weatherCondition === "scattered clouds") || (weatherCondition === "broken clouds")){
+            weatherImage = <img src={clouds} alt="clouds"/>
+        }
+        if ((weatherCondition === 'shower rain') || (weatherCondition === 'rain') || (weatherCondition === 'light rain')
+            || (weatherCondition === 'light intensity drizzle') ||(weatherCondition === 'moderate rain')){
+            weatherImage = <img src={rain} alt="rain"/>
+        }
+        if (weatherCondition === 'thunderstorm'){
+            weatherImage = <img src={thunderstorm}  alt="thunderstorm"/>
+        }
+        if ((weatherCondition === 'light snow') || (weatherCondition === 'snow')){
+            weatherImage = <img src={snow} alt="snow"/>
+        }
+        if (weatherCondition === 'mist'){
+            weatherImage = <img src={mist} alt="mist"/>
+        }
+        if (weatherCondition === 'rain and snow'){
+            weatherImage = <img src={snow_and_rain} alt="snow_and_rain"/>
+        }
+
+            return (
+            <div className="container">
+                <h3>{cityName}, {countryName}</h3>
+                {weatherImage}
+                <div className="temp">
+                    {temperature} {this.state.isCelsius ? '°C' : '°F'}
+                    <button onClick={this.handleClick}>
+                        Click to convert temperature to Fahrenheit
+                    </button>
+                </div>
+            </div>
+        );
+
+    }
+}
+
+
+
+
+class Result extends Component {
+    render() {
+        return (
+            <div>
+                <header className="header">React Weather App</header>
+
+                <WeatherApp/>
+
+                <footer className="footer"><p>Handmade by me.</p></footer>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<Result/>, document.getElementById("root"))
